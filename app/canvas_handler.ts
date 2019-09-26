70d082812540cbcd57f89ef5d4a3277d2d07dc6f
Beau Blyth <teknopants@gmail.com>
2019-09-26 15:36:27 -0700

    nicer colors. Crisp rendering

--- app/canvas_handler.ts
+++ app/canvas_handler.ts
@@ -4,13 +4,12 @@ export class CanvasHandler {
 
 	constructor(currentImageDiv: HTMLElement) {
 		this.currentImageDiv = currentImageDiv;
+	}
+
+	public Load() {
 		this.currentImageDiv.addEventListener('onmousedown', this.clickOnImage);
 		this.currentImageDiv.addEventListener('onmouseover', this.hoverOverImage);
 	}
-
-	// public Load() {
-	// 	this.currentImageDiv.addEventListener('onmousedown', this.clickOnImage);
-	// }
 
 	private hoverOverImage() {
 		console.log('hovering over image');
